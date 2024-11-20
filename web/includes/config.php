82ece2f7dcf402aeb3cf4510b2fa95ab3b1fd6b2
KRJaeh0 <jaeho6627@kakao.com>
2024-11-20 14:32:26 +0900

    new user update

--- web/includes/config.php
+++ web/includes/config.php
@@ -4,8 +4,8 @@ define('UPLOAD_DIR', __DIR__ . '/../uploads/');
 function getDBConnection() {
     $host = '127.0.0.1';  // 데이터베이스 호스트
     $dbname = 'vulnerable_db';  // 데이터베이스 이름
-    $username = 'root';  // 데이터베이스 사용자
-    $password = '1q2w3e4r!@#$';  // 데이터베이스 비밀번호
+    $username = 'webuser';  // 데이터베이스 사용자
+    $password = '비밀번호';  // 데이터베이스 비밀번호
     
     try {
         $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
